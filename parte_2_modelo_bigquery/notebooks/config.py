c9dd49565ac34e68a5e5699c5b0ade934a40bd17
Guille-33 <guillermo.lucas.glc@gmail.com>
2026-09-06 23:46:42 +0200

    feature: data creation

--- parte_2_modelo_bigquery/notebooks/config.py
+++ parte_2_modelo_bigquery/notebooks/config.py
@@ -3,7 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-# Configuración
+# Configuración dataset
 PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 DATASET_ID = os.getenv("BQ_DATASET_ID")
 CREDENTIALS_PATH = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+# Configuración generador data
+N_CLIENTES=500
+N_PRODUCTOS=70
+N_PEDIDOS=2000
